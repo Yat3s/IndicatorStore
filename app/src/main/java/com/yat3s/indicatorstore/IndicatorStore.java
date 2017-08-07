ec51146df0d962a66a672cb8f61b747ef59b44c2
Yat3s <4645643@qq.com>
2017-08-07 20:31:08 +0800

    Added first indicator location

---
+++ app/src/main/java/com/yat3s/indicatorstore/IndicatorStore.java
@@ -0,0 +1,10 @@
+package com.yat3s.indicatorstore;
+
+/**
+ * Created by Yat3s on 07/08/2017.
+ * Email: hawkoyates@gmail.com
+ * GitHub: https://github.com/yat3s
+ */
+public class IndicatorStore {
+    public static final String LOTTIE_CIRCLE_ORANGE = "circle_stroke_white.json";
+}
--- app/src/main/java/com/yat3s/indicatorstore/MainActivity.java
+++ app/src/main/java/com/yat3s/indicatorstore/MainActivity.java
@@ -1,16 +1,49 @@
 package com.yat3s.indicatorstore;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.yat3s.chopin.ChopinLayout;
+import com.yat3s.chopin.indicator.LottieIndicator;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int COMPLETE_DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ChopinLayout chopinLayout = (ChopinLayout) findViewById(R.id.chopin_layout);
+        LottieIndicator headerIndicator = new LottieIndicator(this, IndicatorStore.LOTTIE_CIRCLE_ORANGE, 0.2f);
+        LottieIndicator footerIndicator = new LottieIndicator(this, IndicatorStore.LOTTIE_CIRCLE_ORANGE, 0.2f);
+        headerIndicator.setBackgroundColor(Color.GREEN);
+        footerIndicator.setBackgroundColor(Color.GREEN);
+
+        chopinLayout.setRefreshHeaderIndicator(headerIndicator);
+        chopinLayout.setLoadingFooterIndicator(footerIndicator);
+        chopinLayout.setOnRefreshListener(new ChopinLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                chopinLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        chopinLayout.refreshComplete();
+                    }
+                }, COMPLETE_DELAY);
+            }
+        });
+        chopinLayout.setOnLoadMoreListener(new ChopinLayout.OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                chopinLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        chopinLayout.refreshComplete();
+                    }
+                }, COMPLETE_DELAY);
+            }
+        });
     }
 }
