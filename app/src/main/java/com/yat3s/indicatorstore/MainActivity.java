bb6859209b8ca8d88814a9c9f897349bb693a365
Yat3s <4645643@qq.com>
2017-08-07 19:54:02 +0800

    Init commit

---
+++ app/src/main/java/com/yat3s/indicatorstore/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yat3s.indicatorstore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yat3s.chopin.ChopinLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
